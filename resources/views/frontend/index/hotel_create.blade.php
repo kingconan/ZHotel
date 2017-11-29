c1f0edd7c651b1ca168ba00204478e3ffc5fd9cb
jingang <jingang@117go.com>
2017-11-30 00:02:52 +0800

    support js sdk of qiniu

--- resources/views/frontend/index/hotel_create.blade.php
+++ resources/views/frontend/index/hotel_create.blade.php
@@ -660,6 +660,79 @@
         return null;
     }
 </script>
+<script>
+    var uploader = null;
+    function init_7n(){
+        if(uploader != null) return;
+        console.log("init 7n");
+        uploader = Qiniu.uploader({
+            runtimes: 'html5,flash,html4',      // 上传模式，依次退化
+            browse_button: 'pickfiles',
+            uptoken_url: '/qiniu/token',
+            get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的uptoken
+            domain: 'http://oytstg973.bkt.clouddn.com',     // bucket域名，下载资源时用到，必需
+            //container: 'container',             // 上传区域DOM ID，默认是browser_button的父元素
+            max_file_size: '100mb',             // 最大文件体积限制
+            flash_swf_url: 'https://cdn.staticfile.org/plupload/2.1.9/Moxie.swf',  //引入flash，相对路径
+            max_retries: 3,                     // 上传失败最大重试次数
+            dragdrop: false,                     // 开启可拖曳上传
+            //drop_element: 'container',          // 拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+            chunk_size: '4mb',                  // 分块上传时，每块的体积
+            auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
+            init: {
+                'FilesAdded': function(up, files) {
+                    plupload.each(files, function(file) {
+                        // 文件添加进队列后，处理相关的事情
+                    });
+                },
+                'BeforeUpload': function(up, file) {
+                    // 每个文件上传前，处理相关的事情
+                },
+                'UploadProgress': function(up, file) {
+                    // 每个文件上传时，处理相关的事情
+                },
+                'FileUploaded': function(up, file, info) {
+                    // 每个文件上传成功后，处理相关的事情
+                    // 其中info.response是文件上传成功后，服务端返回的json，形式如：
+                    // {
+                    //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+                    //    "key": "gogopher.jpg"
+                    //  }
+                    //查看简单反馈
+                    console.log("file uploaded");
+                    console.log(info);
+                    var domain = up.getOption('domain');
+                    var res = JSON.parse(info);
+                    var sourceLink = domain +"/"+ res.key; //获取上传成功后的文件的Url
+//                    console.log("FileUploaded");
+                    console.log(sourceLink);
+                    var image = {
+                        url : sourceLink,
+                        tag : "",
+                        status : 1,
+                        created_at : Date.now()
+                    }
+                    if(!hotelList.hotel.images){
+                        hotelList.hotel.images = [];
+                    }
+                    hotelList.hotel.images.splice(0,0,image);
+
+                },
+                'Error': function(up, err, errTip) {
+                    //上传出错时，处理相关的事情
+                },
+                'UploadComplete': function() {
+                    //队列文件处理完毕后，处理相关的事情
+                },
+                unique_names: true
+            }
+        });
+
+        // domain为七牛空间对应的域名，选择某个空间后，可通过 空间设置->基本设置->域名设置 查看获取
+
+        // uploader为一个plupload对象，继承了所有plupload的方法
+    };
+</script>
 {{--<script async defer--}}
         {{--src="http://ditu.google.cn/maps/api/js?key=AIzaSyBJfv6WxdEoTqSgibZDdOL-m-lLWz6UO8E&libraries=geometry,places&callback=cb_map">--}}
 {{--</script>--}}
@@ -905,6 +978,7 @@
             console.log(_id)
         },
         updated :  function(){
+            console.log("updated")
             var swiper = new Swiper('.swiper-container', {
                 navigation: {
                     nextEl: '.swiper-button-next2',
@@ -919,8 +993,10 @@
                 spaceBetween: 0,
                 loop:true
             });
+            init_7n();
         },
         mounted:function(){
+            console.log("mounted")
         },
         methods:{
             check_data : function(){
@@ -1510,78 +1586,6 @@
     }
 
 </script>
-<script>
-    $( document ).ready(function() {
-        console.log( "ready!" );
-        var uploader = Qiniu.uploader({
-            runtimes: 'html5,flash,html4',      // 上传模式，依次退化
-            browse_button: 'pickfiles',
-            uptoken_url: '/qiniu/token',
-            get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的uptoken
-            domain: 'http://oytstg973.bkt.clouddn.com',     // bucket域名，下载资源时用到，必需
-            //container: 'container',             // 上传区域DOM ID，默认是browser_button的父元素
-            max_file_size: '100mb',             // 最大文件体积限制
-            flash_swf_url: 'https://cdn.staticfile.org/plupload/2.1.9/Moxie.swf',  //引入flash，相对路径
-            max_retries: 3,                     // 上传失败最大重试次数
-            dragdrop: false,                     // 开启可拖曳上传
-            //drop_element: 'container',          // 拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
-            chunk_size: '4mb',                  // 分块上传时，每块的体积
-            auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
-            init: {
-                'FilesAdded': function(up, files) {
-                    plupload.each(files, function(file) {
-                        // 文件添加进队列后，处理相关的事情
-                    });
-                },
-                'BeforeUpload': function(up, file) {
-                    // 每个文件上传前，处理相关的事情
-                },
-                'UploadProgress': function(up, file) {
-                    // 每个文件上传时，处理相关的事情
-                },
-                'FileUploaded': function(up, file, info) {
-                    // 每个文件上传成功后，处理相关的事情
-                    // 其中info.response是文件上传成功后，服务端返回的json，形式如：
-                    // {
-                    //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
-                    //    "key": "gogopher.jpg"
-                    //  }
-                    //查看简单反馈
-                    console.log("file uploaded");
-                    console.log(info);
-                    var domain = up.getOption('domain');
-                    var res = JSON.parse(info);
-                    var sourceLink = domain +"/"+ res.key; //获取上传成功后的文件的Url
-//                    console.log("FileUploaded");
-                    console.log(sourceLink);
-                    var image = {
-                        url : sourceLink,
-                        tag : "",
-                        status : 1,
-                        created_at : Date.now()
-                    }
-                    if(!hotelList.hotel.images){
-                        hotelList.hotel.images = [];
-                    }
-                    hotelList.hotel.images.splice(0,0,image);
 
-                },
-                'Error': function(up, err, errTip) {
-                    //上传出错时，处理相关的事情
-                },
-                'UploadComplete': function() {
-                    //队列文件处理完毕后，处理相关的事情
-                },
-                unique_names: true
-            }
-        });
-
-        // domain为七牛空间对应的域名，选择某个空间后，可通过 空间设置->基本设置->域名设置 查看获取
-
-        // uploader为一个plupload对象，继承了所有plupload的方法
-    });
-
-
-</script>
 
 @endsection
