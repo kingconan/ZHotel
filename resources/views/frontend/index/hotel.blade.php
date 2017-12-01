a8cb03367036f90a657ccd88fc210dab87c2054a
jingang <jingang@117go.com>
2017-12-01 17:53:13 +0800

    detect leave

--- resources/views/frontend/index/hotel.blade.php
+++ resources/views/frontend/index/hotel.blade.php
@@ -315,7 +315,7 @@
             <div class="hotel_header" id="hotel_header">
                 <div class="swiper-container" style="height: 432px">
                     <div class="swiper-wrapper">
-                        <div v-for="(image, index) in sorted_covers" class="swiper-slide" style="width: 70%;height:600px">
+                        <div v-for="(image, index) in sorted_covers" class="swiper-slide" style="width: 70%;height:432px">
                             <img style="height: 432px;width: 100%;object-fit: cover;" :src="image.url" />
                         </div>
                     </div>
